a6459ae8f676174aef7d99b95b655ae896a5d77d
jeremydmiller <jeremydmiller@yahoo.com>
2020-01-09 13:42:53 -0600

    Ping Pong with Azure Service Bus

---
+++ PingPongWithAzureServiceBus/Pinger/JasperConfig.cs
@@ -0,0 +1,29 @@
+using Jasper;
+using Jasper.AzureServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Pinger
+{
+    internal class JasperConfig : JasperOptions
+    {
+        public JasperConfig()
+        {
+            // Publish all messages to an Azure Service Bus queue
+            Endpoints
+                .PublishAllMessages()
+                .ToAzureServiceBusQueue("pings");
+            
+            // Listen to incoming messages from an Azure Service Bus
+            // queue
+            Endpoints.ListenToAzureServiceBusQueue("pongs");
+            
+            // Because Jasper rides on top of the built in
+            // .Net Core generic host, you can use the 
+            // IHostedService
+            Services.AddHostedService<PingerService>();
+        }
+    }
+
+}
